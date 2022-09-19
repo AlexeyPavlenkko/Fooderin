2c869cef3f943b4dfcae58f0b66c46544a954a5a
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-19 19:03:59 +0300

    added storyboarded protocol to instantiate Vis from storyboard faster and cleaner

---
+++ Fooderin/Protocols/Storyboarded.swift
@@ -0,0 +1,22 @@
+//
+//  Storyboarded.swift
+//  Fooderin
+//
+//  Created by Алексей Павленко on 19.09.2022.
+//
+
+import UIKit
+
+protocol Storyboarded {
+    static var storyboardName: String { get }
+    static var identifier: String { get }
+    static func instantiate() -> UIViewController
+}
+
+extension Storyboarded where Self: UIViewController {
+    static func instantiate() -> UIViewController {
+        let storyboard = UIStoryboard(name: storyboardName, bundle: .main)
+        return storyboard.instantiateViewController(withIdentifier: identifier) 
+    }
+}
+
