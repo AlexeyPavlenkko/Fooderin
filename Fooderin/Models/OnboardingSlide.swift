6802f6e5f2b59498d812a133670b4af4700d9e45
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-19 18:46:22 +0300

    added OnboardingSlide to keep info about each onboarding slide

---
+++ Fooderin/Models/OnboardingSlide.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingSlide.swift
+//  Fooderin
+//
+//  Created by Алексей Павленко on 19.09.2022.
+//
+
+import Foundation
+
+struct OnboardingSlide {
+    let title: String
+    let description: String
+    let imageName: String
+}
