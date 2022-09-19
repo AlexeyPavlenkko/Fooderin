eec440092c9d3c948db12c1b8b6d8530468a97a2
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-19 19:03:22 +0300

    extension for UIColor to have app's main color

---
+++ Fooderin/Extensions/UIColor+.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+.swift
+//  Fooderin
+//
+//  Created by Алексей Павленко on 19.09.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let main = UIColor(named: "main")!
+}
