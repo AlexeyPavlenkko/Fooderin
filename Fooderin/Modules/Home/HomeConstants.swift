0fc4cb1a4dac198d899a001fca0b6ba77fad95e1
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-20 23:23:36 +0300

    added HomeConstants to control cornerRadius and shadow

---
+++ Fooderin/Modules/Home/HomeConstants.swift
@@ -0,0 +1,14 @@
+//
+//  HomeConstants.swift
+//  Fooderin
+//
+//  Created by Алексей Павленко on 20.09.2022.
+//
+
+import Foundation
+
+struct HomeConstants {
+    static let cournerRadius: CGFloat = 10
+    static let shadowRadius: CGFloat = 5
+    static let shadowOpacity: Float = 0.2
+}
