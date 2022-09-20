700c87efabcd029718aee33e5e9a72d11532bcff
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-20 23:24:35 +0300

    FoodCategory model

---
+++ Fooderin/Modules/Home/Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  Fooderin
+//
+//  Created by Алексей Павленко on 20.09.2022.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let id: String?
+    let name: String?
+    let image: String?
+}
