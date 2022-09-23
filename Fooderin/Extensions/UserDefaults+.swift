9a6d49e552f325aa4cb884cc6225fa01807eb494
Alexey Pavlenko <alexvirtuoso1337@gmail.com>
2022-09-23 11:20:05 +0300

    extension UserDefaults.Keys

---
+++ Fooderin/Extensions/UserDefaults+.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaults+.swift
+//  Fooderin
+//
+//  Created by Алексей Павленко on 22.09.2022.
+//
+
+import Foundation
+
+extension UserDefaults {
+    enum Keys {
+        static let onboardingScreenWasShown = "fooderin.onboardingScreen.wasShown"
+    }
+}
